535f45a7f6b04bcecb65836725627804e80cc40c
chami98 <chamikara98@gmail.com>
2022-07-24 15:33:23 +0530

    Created ExportDiagnosisModal component and moved Modal to it

---
+++ src/Components/ExportDiagnosisModal.js
@@ -0,0 +1,106 @@
+import React from "react";
+import Modal from "@mui/material/Modal";
+import CloseIcon from "@mui/icons-material/Close";
+import ExportDiagnosisTable from "./ExportDiagnosisTable";
+import "../App.css";
+import {
+  AppBar,
+  Button,
+  Divider,
+  Typography,
+  Box,
+  Toolbar,
+} from "@mui/material";
+
+const ExportDiagnosisModal = ({
+  symptomChecked,
+  setSymptomChecked,
+  diagnosisModalOpen,
+  setDiagnosisModalOpen,
+}) => {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "536px",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const handleOpen = () => setDiagnosisModalOpen(true);
+  const handleClose = () => setDiagnosisModalOpen(false);
+
+  return (
+    <Modal
+      open={diagnosisModalOpen}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <AppBar
+          sx={{
+            background:
+              "transparent linear-gradient(96deg, #4864F1 0%, #AA55F4 100%) 0% 0% no-repeat padding-box",
+          }}
+        >
+          <Toolbar sx={{ justifyContent: "space-between" }}>
+            <Typography variant="h6" component="div">
+              Diagnose exportieren
+            </Typography>
+            <CloseIcon sx={{ cursor: "pointer" }} onClick={handleClose} />
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+        <Typography variant="body" component="body" className="ModalText">
+          Prüfen Sie die ausgewählte Diagnose vor dem Senden.
+        </Typography>
+        <ExportDiagnosisTable symptomChecked={symptomChecked} />
+        <Box sx={{ marginTop: "51px" }}></Box>
+        <Divider />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            marginTop: "14px",
+            background: "#FAFCFC 0% 0% no-repeat padding-box",
+            borderRadius: 1,
+          }}
+        >
+          <Button
+            variant="outlined"
+            onClick={handleClose}
+            style={{
+              background: "#FFFFFF 0% 0% no-repeat padding-box",
+              border: "1px solid #5A55F4",
+              borderRadius: "21px",
+              opacity: 1,
+              marginRight: "10px",
+            }}
+          >
+            ABBRECHEN
+          </Button>
+          <Button
+            style={{
+              background:
+                "transparent linear-gradient(101deg, #5B55F4 0%, #AC55F4 100%) 0% 0% no-repeat padding-box",
+              boxShadow: "0px 2px 2px #0000003D",
+              borderRadius: "21px",
+              opacity: 1,
+            }}
+            variant="contained"
+            onClick={handleOpen}
+          >
+            DIAGNOSE SENDEN
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
+};
+
+export default ExportDiagnosisModal;
--- src/Components/OutputDataContainer.js
+++ src/Components/OutputDataContainer.js
@@ -10,22 +10,7 @@ import Box from "@mui/material/Box";
 import { AppBar, Button, Divider, Paper, Stack, Toolbar } from "@mui/material";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import DetectedDiagnoses from "./DetectedDiagnoses";
-import Modal from "@mui/material/Modal";
-import CloseIcon from "@mui/icons-material/Close";
-import ExportDiagnosisTable from "./ExportDiagnosisTable";
-import "../App.css";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "536px",
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+import ExportDiagnosisModal from "./ExportDiagnosisModal";
 
 const OutputDataContainer = ({
   symptom,
@@ -152,74 +137,11 @@ const OutputDataContainer = ({
           </Box>
         </Box>
       </Card>
-
-      <Modal
-        open={diagnosisModalOpen}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <AppBar
-            sx={{
-              background:
-                "transparent linear-gradient(96deg, #4864F1 0%, #AA55F4 100%) 0% 0% no-repeat padding-box",
-            }}
-          >
-            <Toolbar sx={{ justifyContent: "space-between" }}>
-              <Typography variant="h6" component="div">
-                Diagnose exportieren
-              </Typography>
-              <CloseIcon sx={{ cursor: "pointer" }} onClick={handleClose} />
-            </Toolbar>
-          </AppBar>
-          <Toolbar />
-          <Typography variant="body" component="body" className="ModalText">
-            Prüfen Sie die ausgewählte Diagnose vor dem Senden.
-          </Typography>
-          <ExportDiagnosisTable symptomChecked={symptomChecked} />
-          <Box sx={{ marginTop: "51px" }}></Box>
-          <Divider />
-
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "flex-end",
-              marginTop: "14px",
-              background: "#FAFCFC 0% 0% no-repeat padding-box",
-              borderRadius: 1,
-             
-            }}
-          >
-            <Button
-              variant="outlined"
-              style={{
-                background: "#FFFFFF 0% 0% no-repeat padding-box",
-                border: "1px solid #5A55F4",
-                borderRadius: "21px",
-                opacity: 1,
-                marginRight:"10px"
-              }}
-            >
-              ABBRECHEN
-            </Button>
-            <Button
-              style={{
-                background:
-                  "transparent linear-gradient(101deg, #5B55F4 0%, #AC55F4 100%) 0% 0% no-repeat padding-box",
-                boxShadow: "0px 2px 2px #0000003D",
-                borderRadius: "21px",
-                opacity: 1,
-              }}
-              variant="contained"
-              onClick={handleOpen}
-            >
-              DIAGNOSE SENDEN
-            </Button>
-          </Box>
-        </Box>
-      </Modal>
+      <ExportDiagnosisModal
+        symptomChecked={symptomChecked}
+        diagnosisModalOpen={diagnosisModalOpen}
+        setDiagnosisModalOpen={setDiagnosisModalOpen}
+      />
     </Box>
   );
 };
